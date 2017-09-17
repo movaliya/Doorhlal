57c5f71825709de268ed2e819d825e7242484856
kaushik <kaushik.movaliya48@gmail.com>
2017-09-17 19:14:50 +0530

    Add new cell

---
+++ HalalMeatDelivery/NewNearbyCell.h
@@ -0,0 +1,20 @@
+//
+//  NewNearbyCell.h
+//  HalalMeatDelivery
+//
+//  Created by kaushik on 17/09/17.
+//  Copyright © 2017 kaushik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewNearbyCell : UITableViewCell
+{
+    
+}
+@property (strong, nonatomic) IBOutlet UIImageView *RestIMG;
+@property (strong, nonatomic) IBOutlet UILabel *RestName_LBL;
+@property (strong, nonatomic) IBOutlet UILabel *Address_LBL;
+@property (strong, nonatomic) IBOutlet UILabel *Distance_LBL;
+
+@end
---
+++ HalalMeatDelivery/NewNearbyCell.m
@@ -0,0 +1,24 @@
+//
+//  NewNearbyCell.m
+//  HalalMeatDelivery
+//
+//  Created by kaushik on 17/09/17.
+//  Copyright © 2017 kaushik. All rights reserved.
+//
+
+#import "NewNearbyCell.h"
+
+@implementation NewNearbyCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
